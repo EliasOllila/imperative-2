66f08d5e1bd6259d4b9364fe9c434e6240aa4772
eliasollila <elias.ollila@syk.fi>
2022-08-19 11:18:51 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a=3;
+        int b=6;
+        if(a%2==0&&b%2==0)
+            System.out.println(a*a+b*b);
+        else
+            System.out.println(a+b);
+    }
+}
